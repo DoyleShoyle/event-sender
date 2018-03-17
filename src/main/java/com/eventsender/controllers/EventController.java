ebce2524bc47e8aae936095d1c44a4ea281ece3e
Brian Doyle <briandoyle426@gmail.com>
2018-03-17 18:50:01 -0500

    Added Remove Events Added Event Data

--- src/main/java/com/eventsender/controllers/EventController.java
+++ src/main/java/com/eventsender/controllers/EventController.java
@@ -1,43 +1,58 @@
 package com.eventsender.controllers;
 
+import com.eventsender.models.Event;
+import com.eventsender.models.EventData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.ArrayList;
 
 /**
  * Created by Brian Doyle
  */
 @Controller
-@RequestMapping("events")
+@RequestMapping("event")
 public class EventController {
 
-    static ArrayList<String> events = new ArrayList<>();
-
-    //Request Path: /events
+    // Request path: /event
     @RequestMapping(value = "")
     public String index(Model model) {
 
-        model.addAttribute("events", events);
-        model.addAttribute("title", "Event Sender");
-        return "events/index";
+        model.addAttribute("events", EventData.getAll());
+        model.addAttribute("title", "My Events");
+
+        return "event/index";
     }
 
     @RequestMapping(value = "add", method = RequestMethod.GET)
-    public String displayAddEventForm(Model model){
-        model.addAttribute("title", "Add Event" );
-        return "events/add";
-
+    public String displayAddEventForm(Model model) {
+        model.addAttribute("title", "Add Event");
+        return "event/add";
     }
 
     @RequestMapping(value = "add", method = RequestMethod.POST)
-    public String processAddEventForm(@RequestParam String eventName){
-    events.add(eventName);
-
-    //Redirect to /events
-    return "redirect:";
+    public String processAddEventForm(@ModelAttribute Event newEvent) {
+        EventData.add(newEvent);
+        return "redirect:";
     }
+
+    @RequestMapping(value = "remove", method = RequestMethod.GET)
+    public String displayRemoveEventForm(Model model) {
+        model.addAttribute("events", EventData.getAll());
+        model.addAttribute("title", "Remove Event");
+        return "event/remove";
+    }
+
+    @RequestMapping(value = "remove", method = RequestMethod.POST)
+    public String processRemoveEventForm(@RequestParam int[] eventIds) {
+
+        for (int eventId : eventIds) {
+            EventData.remove(eventId);
+        }
+
+        return "redirect:";
+    }
+
 }
